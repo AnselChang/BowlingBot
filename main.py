1d2a5e043d4b28d768c19c469a496952f8e1b4c2
Ansel Chang <ansel.chang@yahoo.com>
2023-09-19 00:40:11 -0400

    basic demo of commands

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from dates_table import DatesTable
 from enums import Commitment
 from session_bowlers_table import SessionBowlersTable
 from sql_table import SqlTable
+import passwords
 
+import discord
+from discord.ext import commands
+from discord import app_commands
 import sqlite3
+
+CHANNEL_BOT_TEST = 1153536156651233341
+
 con = sqlite3.connect("bowling.db")
 cur = con.cursor()
 
@@ -21,11 +28,61 @@ session.createTable()
 
 john = bowlers.insert("John", "Doe", "email@com", "jd#1234", Commitment.ROSTERED)
 jane = bowlers.insert("Jane", "Doe", "email@com", "jd#1234", Commitment.SUB)
-
-john.log()
-
 session.addBowler(john)
 
-john.log()
 
-session.log()
+MY_GUILD = discord.Object(id=1012052441757397062)
+
+class MyClient(discord.Client):
+    def __init__(self, intents: discord.Intents):
+        super().__init__(command_prefix='$', intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+# GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+client = MyClient(intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+
+@client.tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+
+
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
+client.run(passwords.TOKEN)
